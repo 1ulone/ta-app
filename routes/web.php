268ca1769111b062b2127efca7101716d092bfe7
ulone <42242738+1ulone@users.noreply.github.com>
2024-11-21 16:09:49 +0700

    added comments on web

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/* 
+buat akses fungsi controller dan ngeliat view pake file ini
+tinggal pake kode gini
+Route::get('destinasi file view', [nama Controller::class, 'nama fungsi'])->name('nama rute ini buat nanti dipanggil di controller');
+---------------------------------------------------------------------------------------------------------------------
+*contoh destinasi file view = 
+misal file yang dituju aktivitas.blade.php ada di folder dashboard, berarti nulisnya '/dashboard/aktivitas'
+---------------------------------------------------------------------------------------------------------------------
+*contoh nama rute = bebas tapi kalo mau ada 2 kalimat dipisah pake '.'
+*/
+
 Route::get('/dashboard/aktivitas', [aktivitasController::class, 'mahasiswa_index'])->name('dashboard.aktivitas');
